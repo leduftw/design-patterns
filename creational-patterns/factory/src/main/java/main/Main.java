a95d1cc5e6c86c465320c51cbfb17e6470716142
Dusko Sretenovic <dsretenovic555@gmail.com>
2020-09-26 18:54:07 +0200

    Add comment for Factory pattern testing

--- creational-patterns/factory/src/main/java/main/Main.java
+++ creational-patterns/factory/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         DatabaseDriver driver = Factory.getDriver("PostgreSQL");
-        System.out.println(driver);
+        System.out.println(driver);  // prints "PostgreSQL driver"
     }
 
 }
